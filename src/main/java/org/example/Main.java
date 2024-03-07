42b89769019d3bbdbc79a1beb97e558c2604c0c2
Новикова Галина Федоровна <gfnovikova@vtb.ru>
2024-03-07 23:48:44 +0300

    Account_plus_test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("******************");
+        System.out.println("Hello and welcome!");
+
+        Account Acc1;
+        Acc1 = new Account("Иван Иванов");
+        Acc1.setBalances(CurTypes.RUR,100);
+        Acc1.printBalances();
+        Acc1.setName("Вася Пупкин");
+        Acc1.setBalances(CurTypes.EUR,25);
+        Acc1.setBalances(CurTypes.RUR,170);
+        Acc1.printBalances();
+        Acc1.setName("Конь Впальто");
+        Acc1.setBalances(CurTypes.CNY,99);
+        Acc1.printBalances();
+        //Acc1.undo();
+        //Acc1.printBalances();
+    }
+}
---
+++ src/test/java/org/example/AccountTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+    @Test
+    void getState() {
+    }
+}
